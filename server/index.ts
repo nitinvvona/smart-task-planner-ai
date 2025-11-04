908a2a31e8101239b62b055ebf36b04f69c2a44e
nitinvvoona <nitin.voona@gmail.com>
2025-11-04 19:20:04 +0530

    Fix Vercel deployment: separate dev dependencies from production bundle

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic } from "./static";
+import { log } from "./utils";
 
 const app = express();
 app.use(express.json());
@@ -50,6 +51,8 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    // Dynamic import to avoid bundling Vite in production
+    const { setupVite } = await import("./vite");
     await setupVite(app, server);
   } else {
     serveStatic(app);
